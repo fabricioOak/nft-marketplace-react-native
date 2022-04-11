99643b05652f72d0c730bee65df2421931662bbc
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-04-11 01:02:42 -0300

    main import for components

---
+++ components/index.js
@@ -0,0 +1,12 @@
+import HomeHeader from './HomeHeader';
+import NFTCard from './NFTCard';
+import FocusedStatusBar from './FocusedStatusBar';
+import { CircleButton, RectangularButton } from './AppButtons';
+
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  CircleButton,
+  RectangularButton,
+}
