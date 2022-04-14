dd1c58c9b15f9dc3948818bde0fb039cb50c9733
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-04-14 00:35:36 -0300

    new details page components

--- components/index.js
+++ components/index.js
@@ -2,6 +2,8 @@ import HomeHeader from './HomeHeader';
 import NFTCard from './NFTCard';
 import FocusedStatusBar from './FocusedStatusBar';
 import { CircleButton, RectangularButton } from './AppButtons';
+import {DetailsDesc} from './DetailsDesc';
+import {DetailsBid} from './DetailsBid';
 
 export {
   HomeHeader,
@@ -9,4 +11,6 @@ export {
   FocusedStatusBar,
   CircleButton,
   RectangularButton,
+  DetailsDesc,
+  DetailsBid
 }
