b6f03deec043d7610d439c335e9b12cd99d316d4
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-04-16 03:58:38 -0300

    add search function

--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,39 @@
+import React, { useState } from "react";
 import { View, FlatList, SafeAreaView } from "react-native";
 
 import { COLORS, NFTData } from "../constants";
-
 import { NFTCard, FocusedStatusBar, HomeHeader } from "../components";
 
 const Home = () => {
+	const [nftData, setNftData] = useState(NFTData);
+
+  const handleSearch = (value) => {
+    if (value.length === 0) {
+      setNftData(NFTData);
+    }
+
+    const filteredData = NFTData.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if (filteredData.length === 0) {
+      setNftData(NFTData);
+    } else {
+      setNftData(filteredData);
+    }
+  };
+
 	return (
 		<SafeAreaView style={{ flex: 1 }}>
 			<FocusedStatusBar backgroundColor={COLORS.primary} />
 			<View style={{ flex: 1 }}>
 				<View style={{ zIndex: 0 }}>
 					<FlatList
-						data={NFTData}
+						data={nftData}
 						keyExtractor={(item) => item.id}
 						showsVerticalScrollIndicator={false}
 						renderItem={({ item }) => <NFTCard data={item} />}
-						ListHeaderComponent={<HomeHeader />}
+						ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
 					/>
 				</View>
 				<View
